edf014f3a6c29701084ddb2807859d40e90213ed
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-01-31 13:17:24 +0700

    first commit

---
+++ app/src/main/java/toong/vn/androidopenwebview/MainActivity.java
@@ -0,0 +1,24 @@
+package toong.vn.androidopenwebview;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_open_web_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.google.com.vn/"));
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
